729a9c446903038e0488193f44dd725dc47f5019
Alex Cline <clinealex10@gmail.com>
2019-05-13 08:47:54 -0500

    Initial Upload

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class AppManager : MonoBehaviour
+{
+    [SerializeField] private Image fadeImage = null;    // Image that fades in and out between the scene.
+    private Animator fadeAnimator = null;               // Animtor that fades the fadeImage.
+
+    public static string currentEventName = "";     // Name of the event begin attended.
+
+    [SerializeField] private TextMeshProUGUI headerText = null;         // Text on screen for the header.
+    [SerializeField] private GameObject eventInput = null;              // Entry field object.
+    [SerializeField] private TextMeshProUGUI eventName = null;          // Entry field for the events.
+    [SerializeField] private TextMeshProUGUI placeHolderText = null;    // Entry field for the events.
+    [SerializeField] private Button changeEventButton = null;           // Used to change the event.
+
+    void Awake()
+    {
+        // Sets up the fadeImage for the beginning of the scene.
+        fadeImage.enabled = true;
+        fadeAnimator = fadeImage.gameObject.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        // Sets up the current the event information if any.
+        if (currentEventName == "")
+            UpdateEventInputActive(true);
+        else
+            UpdateEventName(currentEventName);
+    }
+
+    /// <summary> Enables the input field for events. </summary>
+    public void UpdateEventInputActive(bool isActive)
+    {
+        eventInput.SetActive(isActive);
+        changeEventButton.gameObject.SetActive(!isActive);
+    }
+
+    /// <summary>  Updates the name of the event. </summary>
+    /// <param name="newEventName"> String of the new event name. </param>
+    public void UpdateEventName(string newEventName)
+    {
+        currentEventName = newEventName;
+        eventName.text = newEventName;
+        placeHolderText.enabled = false;
+        headerText.text += currentEventName + "!";
+        UpdateEventInputActive(false);
+    }
+
+    /// <summary> Calls the coroutine to start asynchronous loading. </summary>
+    public void BeginAsyncLoading()
+    {
+        // Begins the Fade-In animation.
+        fadeAnimator.SetBool("ShouldFadeIn", true);
+
+        // Begins the coroutine to reload the scene.
+        StartCoroutine(ReloadScene());
+    }
+
+    /// <summary> Reloads scene after data entry. </summary>
+    private IEnumerator ReloadScene()
+    {
+        // Wait for the Fade-In to finish.
+        yield return new WaitForSeconds(1f);
+
+        // Begins asynchronous loading.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+
+        // If the asynchronous loading isn't done...
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DataManager : MonoBehaviour
+{
+    private string savePath = "";  // String for the path of the json file.
+
+    [SerializeField] private TextMeshProUGUI nameField = null;          // Entry field for the name.
+    [SerializeField] private TextMeshProUGUI employeeIDField = null;    // Entry field for the employee ID.
+
+    private void Awake()
+    {
+        savePath = Application.dataPath + "/saveFile.json";
+    }
+
+    /// <summary> Information pulled from the entry field. </summary>
+    public void SubmittedInfo()
+    {
+        EnteredData newData = new EnteredData(nameField.text, employeeIDField.text);    // Saves the data entered to a data container.
+
+        string newJson = JsonUtility.ToJson(newData);   // Converts data to json formatted strings.
+        string oldJson = "";                            // Variable to store old json data.
+
+        try
+        {
+            oldJson = File.ReadAllText(savePath);   // Loads previosuly saved data if any.
+        }
+        catch (FileNotFoundException)   // If no saved data found...
+        {
+            Debug.Log("saveFile.json not found.");
+        }
+        if (oldJson != "")
+            oldJson += "\n";
+
+        File.WriteAllText(savePath, oldJson + newJson); // Saves data string to json file.
+
+        GetComponent<AppManager>().BeginAsyncLoading(); // Begins reloading scene.
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Send json data to firebase.
+
+        if (File.Exists(savePath))
+            File.Delete(savePath);              // Deletes the json file.
+        if (File.Exists(savePath + ".meta"))
+            File.Delete(savePath + ".meta");    // Deletes the json meta data file.
+    }
+
+    [Serializable]
+    private class EnteredData
+    {
+        public string dateTime;         // Date and time that it was submitted.
+        public string eventAttended;    // Event that was attended.
+        public string employeeName;     // Name of the person.
+        public string employeeID;       // ID number of the person attending.
+
+        /// <summary> Data the user entered. </summary>
+        /// <param name="userName"> Name of the user. </param>
+        /// <param name="userID"> ID number of the user. </param>
+        public EnteredData(string userName, string userID)
+        {
+            dateTime = DateTime.Now.ToString();
+            eventAttended = AppManager.currentEventName;
+            employeeName = userName;
+            employeeID = userID;
+        }
+    }
+}
